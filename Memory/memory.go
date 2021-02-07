1f64654e520d5c14f8d490b48b78e4576923e9f3
Procok <szeplakig91@gmail.com>
2021-02-07 22:36:02 +0100

    Fix memory method names

--- Memory/memory.go
+++ Memory/memory.go
@@ -1,36 +1,39 @@
 package memory
 
-type Byte = uint8  // Byte type 6502
-type Word = uint16 // Word type 6502
-
 // The Memory structure of the 6502
 type Memory struct {
 	Data []Byte
 }
 
+// Reset the memory
 func (memory *Memory) Reset() {
 	memory.Data = make([]Byte, 0xFFFF)
 }
 
+// Create a new memory
 func NewMemory() Memory {
 	memory := Memory{}
 	memory.Reset()
 	return memory
 }
 
+// Write a Byte to the memory at address
 func (memory *Memory) WB(address Word, value Byte) {
 	memory.Data[address%0xFFFF] = value
 }
 
+// Write a Word to the memory at address (big endian)
 func (memory *Memory) WW(address Word, value Word) {
 	memory.Data[address%0xFFFF] = Byte(value)
 	memory.Data[(address+1)%0xFFFF] = Byte(value >> 8)
 }
 
+// Read a Byte from memory at address
 func (memory *Memory) RB(address Word) Byte {
 	return memory.Data[address%0xFFFF]
 }
 
+// Read a Word from memory at address
 func (memory *Memory) RW(address Word) Word {
 	return Word(memory.Data[address%0xFFFF]) | Word(memory.Data[(address+1)%0xFFFF])<<8
 }
---
+++ Memory/types.go
@@ -0,0 +1,4 @@
+package memory
+
+type Byte = uint8  // Byte type 6502
+type Word = uint16 // Word type 6502
